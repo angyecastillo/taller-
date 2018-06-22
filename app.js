ca216c96b91426986796240e9214e893e9746dbb
angyecastillo <lindacasags@hotmail.com>
2018-06-22 17:54:49 -0500

    prueba

---
+++ app.js
@@ -0,0 +1,15 @@
+const numero1 = 9824;
+const numero2 = 777;
+const numero3 = 36325;
+
+add =(numero1,numero2) =>{
+  const result =numero1 + numero2;
+  console.log(result);
+  multiply (result, numero3);
+}
+multiply =(result, numero3) => {
+  const resultMultiply = result * numero3;
+  console.log(resultMultiply);
+  return resultMultiply;
+}
+add (numero1, numero2);
